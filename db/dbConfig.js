ef901023c5dfbdb12ae92fe7ae47b7eb65ce8eec
Angie Munoz <angiepmunoz@gmail.com>
2022-02-11 13:10:12 -0500

    updated dbconfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,20 +1,47 @@
-// this is the package we installed that allows us to connect to our postgres db
-const pgp = require('pg-promise')();
-// allows us to access our .env variables
-require('dotenv').config();
+// // this is the package we installed that allows us to connect to our postgres db
+// const pgp = require('pg-promise')();
+// // allows us to access our .env variables
+// require('dotenv').config();
 
-// this is the object we are passing to pr-promise so we can connect to the proper db
-const cn = {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-};
+// // this is the object we are passing to pr-promise so we can connect to the proper db
+// const cn = {
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     database: process.env.PG_DATABASE,
+//     user: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+// };
 
-//  database is now our db i.e anime_dev
+// //  database is now our db i.e anime_dev
 
 
-const database = pgp(cn);
+// const database = pgp(cn);
 
-module.exports = database;
+// module.exports = database;
+
+const pgp = require("pg-promise")();
+require("dotenv").config();
+
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+    };
+
+// alt from express docs
+// var db = pgp('postgres://username:password@host:port/database')
+
+const db = pgp(cn);
+
+module.exports = db;
